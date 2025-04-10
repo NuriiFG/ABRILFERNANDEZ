40929c811aa3c8d5b07d067e11d3848190ebcbfa
NuriiFG <nuriafergo9@gmail.com>
2025-04-10 11:22:55 +0200

    primerComit

---
+++ AbrilFernandez/src/abrilfernandez/Clase1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package abrilfernandez;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+
+    public void nombre(){
+        System.out.println("Nuria Fernández González");
+    }
+
+}
