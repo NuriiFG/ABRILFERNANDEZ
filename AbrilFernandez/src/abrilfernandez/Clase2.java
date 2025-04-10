7eb8e283c38ac7eae20ed7d2ce7044e1f9764ead
NuriiFG <nuriafergo9@gmail.com>
2025-04-10 11:28:55 +0200

    segundoCommit

---
+++ AbrilFernandez/src/abrilfernandez/Clase2.java
@@ -0,0 +1,9 @@
+
+package abrilfernandez;
+
+public class Clase2 {
+    public void rama(){
+        System.out.println("Estoy en la rama Producción");
+        System.out.println("Nuria Fernández González");
+    }
+}
